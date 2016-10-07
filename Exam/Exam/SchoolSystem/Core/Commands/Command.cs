e1f7a12425223ba2fd427e0936e8a97df73ff05f
Петя Павлова <dobromirbogatev@gmail.com>
2016-10-07 18:14:35 +0300

    Last touches to exam

--- Exam/Exam/SchoolSystem/Core/Commands/Command.cs
+++ Exam/Exam/SchoolSystem/Core/Commands/Command.cs
@@ -3,6 +3,9 @@
     using System.Collections.Generic;
     using Contracts;
 
+    /// <summary>
+    /// Command that can be executed
+    /// </summary>
     public abstract class Command : ICommand
     {
         public abstract string Execute(IList<string> parameters);
--- Exam/Exam/SchoolSystem/Models/Student.cs
+++ Exam/Exam/SchoolSystem/Models/Student.cs
@@ -2,10 +2,10 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using Enumerations;
-    using System.Threading;
     using System.Globalization;
+    using System.Linq;
+    using System.Threading;
+    using Enumerations;
 
     public class Student
     {
